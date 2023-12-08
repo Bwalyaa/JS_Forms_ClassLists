0191af8bc8f1b4657c45f7e384f803d36077a664
Bwalyaa <bwalya@hotmail.de>
2023-12-08 14:14:54 +0100

    forms1_1

---
+++ forms_js_level1_1/assets/js/main.js
@@ -0,0 +1,9 @@
+
+ 
+
+function double(){
+    const input = document.querySelector("#number").value
+    let output = document.querySelector(".output")
+    output.innerHTML = input * 2;
+    console.log(input);
+}
